cfc4d257f3dc4835e4e2935ff1e02fc816e174e0
Rafael Torres <33812794+bitman26@users.noreply.github.com>
2024-05-06 07:44:57 -0300

    Update nodestatus.php

--- Node_Automation/apache/www/nodestatus.php
+++ Node_Automation/apache/www/nodestatus.php
@@ -1,3 +1,15 @@
+<?php
+session_start();
+
+// Verifica se o usuário não está autenticado
+if (!isset($_SESSION['authenticated']) || $_SESSION['authenticated'] !== true) {
+    // Redireciona para a página de login
+    header("Location: index.php");
+    exit();
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,25 +20,35 @@
 </head>
 <body>
 
+
+<!-- Barra de navegação -->
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <a class="navbar-brand" href="#">F5 Node Status</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
+        <ul class="navbar-nav mr-auto">
+            <!-- Links da barra de navegação -->
             <li class="nav-item">
-                <a class="nav-link" href="index.html">Home</a>
+                <a class="nav-link" href="home.php">Home</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="node.html">Node Manager</a>
+                <a class="nav-link" href="node.php">Node Manager</a>
             </li>
-            <li class="nav-item active">
-                <a class="nav-link" href="nodestatus.php">Node Status <span class="sr-only">(current)</span></a>
+            <li class="nav-item">
+                <a class="nav-link" href="nodestatus.php">Node Status</a>
             </li>
+            <!-- Adicione outros links conforme necessário -->
         </ul>
+        <!-- Botão de logoff -->
+        <form class="form-inline my-2 my-lg-0" action="logout.php" method="post">
+            <button class="btn btn-outline-danger my-2 my-sm-0" type="submit">Logoff</button>
+        </form>
     </div>
 </nav>
+
+
 
 <div class="container mt-5">
     <img src="logo.png" alt="Logo" class="d-block mx-auto mb-4" style="width: 200px;">
